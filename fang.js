4cbe828adc0555afa53abab19ce04154f626289b
James <james@jameshawkins.com>
2019-06-21 09:23:24 -0700

    Remove question

--- fang.js
+++ fang.js
@@ -1,27 +1,16 @@
-// Defang an IP Address
-// Write a function that takes an IP address and returns a defanged version of the IP address.
-// Example: 12.34.56.78 → 12[.]34[.]56[.]78
-//
-// Fang an IP Address
-// Given a defanged IP address, write a function that fangs it back.
-// Example: 12[.]34[.]56[.]78 → 12.34.56.78
-//
-// Valid IPs: 1.1.1.1, 23.43.12.1, 123.56.1.8
-// Invalid IPs: 5433.123.3.2
+function defang(ip){
 
-function defang(ip_address){
+    if (!isValidIP(ip)) return false;
 
-    if (!isValidIP(ip_address)) return false;
-
-    let arr = ip_address.split(".");
+    let arr = ip.split(".");
     let result = arr.join("[.]");
 
     return result;
 }
 
-function fang(ip_address){
+function fang(ip){
 
-    let arr = ip_address.split("[.]");
+    let arr = ip.split("[.]");
     let result = arr.join(".");
 
     return isValidIP(result) ? result : false;
